bbf6c17a70b73b3fa14f6a4d1ed24fece18ac470
Ashwin Rao <ashwinravrao@gmail.com>
2021-02-22 13:38:51 -0600

    Create viewholder and adapter for displaying nearby bicycle shops

---
+++ app/src/main/java/com/ashwinrao/cyclescout/ui/adapter/NearbyShopAdapter.kt
@@ -0,0 +1,34 @@
+package com.ashwinrao.cyclescout.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.ashwinrao.cyclescout.R
+import com.ashwinrao.cyclescout.data.remote.Response
+import com.ashwinrao.cyclescout.databinding.ViewHolderNearbyShopBinding
+
+class NearbyShopAdapter : RecyclerView.Adapter<NearbyShopAdapter.NearbyShopViewHolder>() {
+
+    var data: List<Response.Result> = mutableListOf()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NearbyShopViewHolder =
+        NearbyShopViewHolder(ViewHolderNearbyShopBinding.inflate(LayoutInflater.from(parent.context)))
+
+    override fun onBindViewHolder(holder: NearbyShopViewHolder, position: Int) {
+        val binding = ViewHolderNearbyShopBinding.bind(holder.itemView)
+        val context = binding.root.context
+        binding.shopName.text = data[position].name
+        if (data[position].openingHours.openNow) {
+            binding.openNow.text = context.getString(R.string.open_now)
+            binding.openNow.setTextColor(context.resources.getColor(R.color.green, context.theme))
+        } else {
+            binding.openNow.text = context.getString(R.string.closed_now)
+            binding.openNow.setTextColor(context.resources.getColor(R.color.red, context.theme))
+        }
+        // TODO: Set image thumbnail using Glide
+    }
+
+    override fun getItemCount(): Int = data.size
+
+    inner class NearbyShopViewHolder(binding: ViewHolderNearbyShopBinding) : RecyclerView.ViewHolder(binding.root)
+}
