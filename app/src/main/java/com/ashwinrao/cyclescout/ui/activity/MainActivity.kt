80239aa15202ff7efcc1c3ab92ca0904cda40e75
Ashwin Rao <ashwinravrao@gmail.com>
2021-02-23 15:50:32 -0600

    Replace placeholder with snackbar, lay groundwork for lazy loading

--- app/src/main/java/com/ashwinrao/cyclescout/ui/activity/MainActivity.kt
+++ app/src/main/java/com/ashwinrao/cyclescout/ui/activity/MainActivity.kt
@@ -21,6 +21,7 @@ import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -29,23 +30,25 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback, SwipeRefreshLayout.OnRefreshListener {
 
-    private val TAG = this@MainActivity.javaClass.simpleName
+    private val tag = this@MainActivity.javaClass.simpleName
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var mapFragment: SupportMapFragment
     private lateinit var shopList: RecyclerView
-    private lateinit var shimmer: ShimmerFrameLayout
-    private lateinit var startLocation: LatLng
-
+    private lateinit var shimmerEffect: ShimmerFrameLayout
     private lateinit var adapter: NearbyShopAdapter
+
+    private var snackBar: Snackbar? = null
     private val mainViewModel: MainViewModel by viewModel()
+
+    // Hardcoding in the interest of time; normally would get user input or device location
+    private val startLocation = LatLng(41.88744282963304, -87.65274711534346)   // SRAM HQ in downtown Chicago
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        placeholder = binding.placeholder
-        shimmer = binding.shimmer
+        shimmerEffect = binding.shimmer
         binding.swipeRefreshLayout.setOnRefreshListener(this@MainActivity)
 
         initializeShopList()
@@ -58,17 +61,32 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, SwipeRefreshLayout
     }
 
     private fun initializeShopList() {
-        startShimmer()
+        startLoadingAnimation()
         shopList = binding.recyclerView
         shopList.setHasFixedSize(true)
         shopList.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
         adapter = NearbyShopAdapter()
         shopList.adapter = adapter
+        fetchDataLazily()
+    }
+
+    /**
+     * In order to lazily load the next set of results, we need to listen to shopList's scroll position.
+     * Once we reach the "bottom" of the recycler view, we can fetch the next set of results from the API.
+     * In keeping with separation of concerns, the view model keeps track of page tokens. So the first call
+     * to fetchNearbyShops() will either return the first set of results or the next page. The activity shouldn't
+     * have to know which.
+     */
+    private fun fetchDataLazily() {
+        fetchNearbyShops(startLocation)
+        shopList.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+
+        })
     }
 
     private fun populateShopList(shops: List<NearbySearch.Result>) {
-        stopShimmer()
-        showShopList()
+        stopLoadingAnimation()
+        shopList.visibility = View.VISIBLE
         adapter.data = shops
         adapter.notifyDataSetChanged()
     }
@@ -93,16 +111,17 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, SwipeRefreshLayout
             ).title(this.getString(R.string.label_location)).anchor(0.5f, 0.5f)
         )
         if (marker != null) animateDropPin(marker, 500)
-        fetchNearbyShops(startLocation)
     }
 
     /**
      * Asynchronously fetch the first set of results matching our keyword from the Google Places API.
      * Once we have a response, we must switch contexts to the Main thread in order to perform UI work
      * such as populating our RecyclerView and dropping pins (adding markers) to the map.
+     *
+     * @return success: Boolean (whether the outcome was as expected - fresh data fetched)
      */
-
-    private fun fetchNearbyShops(locationBias: LatLng) {
+    private fun fetchNearbyShops(locationBias: LatLng): Boolean {
+        var success = false
         var message: String? = null
         CoroutineScope(Dispatchers.IO).launch {
             var nearbyShops: NearbySearch? = null
@@ -110,87 +129,49 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, SwipeRefreshLayout
                 nearbyShops = mainViewModel.fetchNearbyShops(locationBias)
             } catch (e: Exception) {
                 message = e.message
-                Log.d(TAG, "fetchNearbyShops: ${e.message}")
+                Log.d(tag, "fetchNearbyShops: ${e.message}")
             }
             withContext(Dispatchers.Main) {
                 if (nearbyShops != null) {
                     when (nearbyShops.status) {
-                        "OK" -> populateShopList(nearbyShops.results)
-                        "ZERO_RESULTS" -> showPlaceholder(errorStatus = false)
-                        else -> showPlaceholder(errorStatus = true)
+                        "OK" -> {
+                            success = true
+                            populateShopList(nearbyShops.results)
+                        }
+                        "ZERO_RESULTS" -> handleNoResults(message = "No matching results found")
+                        else -> handleNoResults(message = "Something unexpected happened. Try again later")
                     }
-                    Log.d(TAG, "fetchNearbyShops: ${nearbyShops.status}")
-                } else {
-                    showPlaceholder(errorStatus = true, message)
+                    Log.d(tag, "fetchNearbyShops: ${nearbyShops.status}")
                 }
+                message?.let { handleNoResults(it) }
             }
         }
+        return success
     }
 
-    private fun showPlaceholder(errorStatus: Boolean, message: String? = null) {
-        if (errorStatus) {
-            placeholder.icon.setImageDrawable(
-                ResourcesCompat.getDrawable(
-                    this.resources,
-                    R.drawable.ic_error,
-                    this.theme
-                )
-            )
-            placeholder.icon.scaleX = 0.8f
-            placeholder.icon.scaleY = 0.8f
-            placeholder.label.text = message ?: this.resources.getString(R.string.label_placeholder_error)
-        } else {
-            placeholder.icon.setImageDrawable(
-                ResourcesCompat.getDrawable(
-                    this.resources,
-                    R.drawable.ic_sad_phone,
-                    this.theme
-                )
-            )
-            placeholder.icon.scaleX = 1f
-            placeholder.icon.scaleY = 1f
-            placeholder.label.text = this.resources.getString(R.string.label_placeholder_no_results)
-        }
-        stopShimmer()
-        hideShopList()
+    private fun handleNoResults(message: String) {
+        stopLoadingAnimation(hideLayout = false)
+        snackBar = Snackbar.make(binding.root, message, Snackbar.LENGTH_INDEFINITE)
+        snackBar?.show()
     }
 
+    private fun startLoadingAnimation() {
+        shimmerEffect.visibility = View.VISIBLE
+        shimmerEffect.startShimmer()
     }
 
-
-    private fun hideShopList() {
-        adapter.data = mutableListOf()
-        adapter.notifyDataSetChanged()
-        placeholder.root.visibility = View.VISIBLE
-    }
-
-    private fun showShopList() {
-        shopList.visibility = View.VISIBLE
-        placeholder.root.visibility = View.GONE
-    }
-
-    private fun startShimmer() {
-        shimmer.visibility = View.VISIBLE
-        shimmer.startShimmer()
-    }
-
-    private fun stopShimmer() {
-        shimmer.stopShimmer()
-        shimmer.visibility = View.GONE
-    }
-
-    private fun clearShopList() {
-        adapter.data = mutableListOf()
-        adapter.notifyDataSetChanged()
-        placeholder.root.visibility = View.GONE
+    private fun stopLoadingAnimation(hideLayout: Boolean = true) {
+        shimmerEffect.stopShimmer()
+        if (hideLayout) shimmerEffect.visibility = View.GONE
     }
 
     override fun onRefresh() {
-        clearShopList()
-        startShimmer()
         fetchNearbyShops(startLocation)
+        snackBar?.dismiss()
 
-        // swipe to refresh will be dismissed when fresh data is bound to the recyclerview adapter or the placeholder is shown
-        adapter.watchData().observe(this@MainActivity) { binding.swipeRefreshLayout.isRefreshing = false }
+        // swipe to refresh will be dismissed when fresh data is bound to the recyclerview adapter
+        adapter.watchData().observe(this@MainActivity) {
+            binding.swipeRefreshLayout.isRefreshing = false
+        }
     }
 }
--- app/src/main/java/com/ashwinrao/cyclescout/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/ashwinrao/cyclescout/viewmodel/MainViewModel.kt
@@ -1,9 +1,14 @@
 package com.ashwinrao.cyclescout.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.ashwinrao.cyclescout.data.remote.response.NearbySearch
 import com.ashwinrao.cyclescout.data.repository.RepositoryImpl
 import com.google.android.gms.maps.model.LatLng
 
 class MainViewModel(private val repo: RepositoryImpl) : ViewModel() {
-    suspend fun fetchNearbyShops(locationBias: LatLng) = repo.fetchNearbyShops(locationBias)
+    var nextPageToken: String? = null
+
+    suspend fun fetchNearbyShops(locationBias: LatLng): NearbySearch? {
+        return repo.fetchNearbyShops(locationBias)
+    }
 }
