f0c7dc5fe42faa9d68375c64d47daa5a9c9af339
Ashwin Rao <ashwinravrao@gmail.com>
2021-02-23 22:18:35 -0600

    Add request/response logging

--- app/src/main/java/com/ashwinrao/cyclescout/data/remote/PlacesClient.kt
+++ app/src/main/java/com/ashwinrao/cyclescout/data/remote/PlacesClient.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import com.ashwinrao.cyclescout.BuildConfig
 import com.ashwinrao.cyclescout.R
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 import retrofit2.Retrofit
@@ -12,18 +13,24 @@ import retrofit2.converter.gson.GsonConverterFactory
 val networkModule = module {
     factory { AuthInterceptor(androidContext()) }
     factory { ConnectivityInterceptor(androidContext()) }
-    factory { provideOkHttpClient(get(), get()) }
+    factory { HttpLoggingInterceptor() }
+    factory { provideOkHttpClient(get(), get(), get()) }
     factory { providePlacesApi(get()) }
     single { provideRetrofit(androidContext(), get()) }
 }
 
-fun provideRetrofit(context: Context, okHttpClient: OkHttpClient): Retrofit {
-    return Retrofit.Builder().baseUrl(context.getString(R.string.places_url)).client(okHttpClient)
+fun provideRetrofit(context: Context, okHttpClient: OkHttpClient): Retrofit =
+    Retrofit.Builder().baseUrl(context.getString(R.string.places_url)).client(okHttpClient)
         .addConverterFactory(GsonConverterFactory.create()).build()
-}
 
-fun provideOkHttpClient(authInterceptor: AuthInterceptor, connectivityInterceptor: ConnectivityInterceptor): OkHttpClient {
-    return OkHttpClient().newBuilder().addInterceptor(authInterceptor).addInterceptor(connectivityInterceptor).build()
-}
+fun provideOkHttpClient(
+    authInterceptor: AuthInterceptor,
+    connectivityInterceptor: ConnectivityInterceptor,
+    loggingInterceptor: HttpLoggingInterceptor
+): OkHttpClient =
+    OkHttpClient().newBuilder()
+        .addInterceptor(authInterceptor)
+        .addInterceptor(connectivityInterceptor)
+        .addInterceptor(loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY)).build()
 
 fun providePlacesApi(retrofit: Retrofit): PlacesApi = retrofit.create(PlacesApi::class.java)
